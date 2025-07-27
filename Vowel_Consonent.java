885f9570a376eb422a08183438639b3ded8c61f2
Sagar <159099764+Sagar6372@users.noreply.github.com>
2025-07-27 12:00:11 +0530

    Add files via upload

--- Vowel_Consonent.java
+++ Vowel_Consonent.java
@@ -1,18 +1,38 @@
-package Program_Pratice;
+package String_Program;
 
 import java.util.Scanner;
 
 public class Vowel_Consonent {
   public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter a Single character :");
-	String c = sc.next();
 	
-	String vowel = "aeiouAEIOU";
-	if (vowel.contains(c))
-		System.out.println(c+ " is a Vowel");
-	else 
-		System.out.println(c+ " is a Consonent");
-		
+	/*
+	System.out.println("Enetr a Word : ");
+	String st = sc.next();
+	int vowel = 0;
+	int consonent = 0;
+	char[] str = st.toLowerCase().toCharArray();
+	
+	for(char ch : str) {
+		if(Character.isLetter(ch)) {
+			if(ch == 'a' || ch == 'e' || ch== 'i' || ch == 'o' || ch == 'u') {
+				vowel ++;
+			} else {
+				consonent++;
+			}
+		}
+	}
+	System.out.println("Number of Vowel is : "+vowel);
+	System.out.println("Number of Consonent is : "+consonent);
+	*/
+	
+	System.out.println("Enter a Character :");
+	char ch = sc.next().toLowerCase().charAt(0);
+	
+	if(ch == 'a' || ch == 'e' || ch=='i' || ch=='o' || ch == 'u') {
+		System.out.println(ch +" is Vowel");
+	} else {
+		System.out.println(ch+" is Consonent");
+	}
 }
 }
