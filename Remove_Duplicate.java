086990c715689f4d57d1013b6f8a3ecd046e9594
Sagar <159099764+Sagar6372@users.noreply.github.com>
2025-07-29 10:59:09 +0530

    Add files via upload

--- Remove_Duplicate.java
+++ Remove_Duplicate.java
@@ -1,38 +1,21 @@
-package Array;
+package String_Program;
 
-import java.util.Arrays;
-import java.util.HashSet;
-
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Remove_Duplicate {
-    public static void main(String[] args) {
-        int nums[] = {34, 454, 221, 5463, 4323, 876, 70, 34, 876};
-
-        Arrays.sort(nums); // Sort the array first
-
-        System.out.println("Duplicate elements are:");
-
-        // Using Normal for loop
-        /*
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
-                System.out.println(nums[i]);
-                
-                // Skip all further duplicates of the same value
-                while (i < nums.length - 1 && nums[i] == nums[i + 1]) {
-                    i++;
-                }
-            }
-        }
-        */
-        
-        // Using HashSet
-        HashSet<Integer> hs = new HashSet<Integer>();
-        for(Integer i : nums) {
-        	if(!hs.add(i))
-        	System.out.println("Duplicates are : "+i);
-        }
+  public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter a Word : ");
+	String st = sc.nextLine();
+    String result = "";
+    
+    for(int i=0; i< st.length(); i++) {
+    	char ch = st.charAt(i);
+    	
+    	if(!result.contains(String.valueOf(ch))) {
+    		result = result +ch;
+    	}
     }
+    System.out.println("After Removing Duplicates : "+result);
 }
-
+}
